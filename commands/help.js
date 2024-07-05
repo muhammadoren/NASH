62527fcf947815f26f0b7496a6817db08b959bd4
muhammadoren <muhamaddoren49@gmail.com>
2024-07-05 14:29:43 +0800

    help.js

--- commands/help.js
+++ commands/help.js
@@ -1,28 +1,19 @@
 module.exports = {
-    name: "help",
-    description: "Beginner's Guide To All Bot Commands",
-    nashPrefix: false,
-    version: "1.0.2",
-    role: 0,
-    cooldowns: 7,
-    aliases: ["help"],
-    execute(api, event, args, prefix) {
-        const commands = global.NashBoT.commands;
-        const { threadID, messageID } = event;
-        
-        let message = "== 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧 ==\n\n";
-        
-        let i = 0;
-        commands.forEach((cmd, name) => {
-            message += `「 ${++i} 」${prefix}${name}\n`;
-        });
-
-        const pageCount = 1; // Adjust this if you have pagination
-
-        const pageText = `\n\n𝗣𝗔𝗚𝗘 »${pageCount}/${pageCount}«`;
-
-        message += pageText;
-
-        api.sendMessage(message, threadID, messageID);
-    }
+  name: 'help',
+  description: 'Displays information about available commands',
+  nashPrefix: false,
+  execute(api, event, args, prefix, commands) {
+    const helpMessage = generateHelpMessage(commands);
+    api.sendMessage(helpMessage, event.threadID);
+  },
 };
+
+function generateHelpMessage(commands) {
+    let helpMessage = 'Here are the available commands:\n\n';
+
+    commands.forEach((command, name) => {
+        helpMessage += `Command: ${name}\nDescription: ${command.description}\n\n`;
+    });
+
+    return helpMessage;
+}
